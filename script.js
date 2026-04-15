794a99d26d314da7241cf694762569ffa9721bda
Ivan Caldarella <gamein3x@gmail.com>
2026-04-15 16:14:08 +0200

    La logica funziona

--- script.js
+++ script.js
@@ -2,15 +2,18 @@
 
 const API_RANDOM_MAIL = "https://flynn.boolean.careers/exercises/api/random/mail";
 
-fetch(API_RANDOM_MAIL)
-    .then((risposta) => {
-        return risposta.json();
-    })
-    .then((jsonData) => {
-        const randomMailList = [];
-        for (i=0; i<10; i++) {
+const randomMailList = [];
+for (i = 0; i < 10; i++) {
+    fetch(API_RANDOM_MAIL)
+        .then((risposta) => {
+            return risposta.json();
+        })
+        .then((jsonData) => {
             let randomMail = jsonData.response;
             console.log(randomMail);
             randomMailList.push(randomMail)
-        }
-    });
+        });
+    };
+
+console.log(randomMailList);
+
